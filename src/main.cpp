99143621169d32923f8656c1c2c7720a0a2aec15
Bobby4925 <122075721+Bobby4925@users.noreply.github.com>
2024-09-24 00:32:41 -0700

    Added Base Auton Importing

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include "main.h"
 #include "lemlib/api.hpp" // IWYU pragma: keep
 #include "config.cpp"
+#include "autons/main.cpp"
 
 /**
  * A callback function for LLEMU's center button.
@@ -61,7 +62,9 @@ void competition_initialize() {}
  * will be stopped. Re-enabling the robot will restart the task, not re-start it
  * from where it left off.
  */
-void autonomous() {}
+void autonomous() {
+	baseAuton(); 
+}
 
 /**
  * Runs the operator control code. This function will be started in its own task
